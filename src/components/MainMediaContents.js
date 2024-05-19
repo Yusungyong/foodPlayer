b4a96bd72952cc0be90fdddfea61ca62595509e7
Yusungyong <su12ng@gmail.com>
2024-05-19 20:46:35 +0900

    cc

--- src/components/MainMediaContents.js
+++ src/components/MainMediaContents.js
@@ -82,7 +82,7 @@ const handleMenuBlock = () => {
             <p onClick={handleScreenBlock}>영상 목록</p>
             </div>
             <video id="videoPlayer" 
-            className="player" controls loop autoPlay playsinline webkit-playsinline 
+            className="player" controls loop autoPlay playsinline muted 
             width='100%' height='100%'
             data-no-fullscree="true">
               Your browser does not support the video tag.
